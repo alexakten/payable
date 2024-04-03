f3dbb86d2206452201dab67be72516fde192c4a2
Alex <alex.akten@outlook.com>
2024-04-03 13:56:49 +0200

    init commit

---
+++ src/app/components/ui/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 flex w-full justify-between bg-red-200 px-4 py-4">
+      <div className="text-2xl font-medium">Darwin</div>
+    </nav>
+  );
+}
