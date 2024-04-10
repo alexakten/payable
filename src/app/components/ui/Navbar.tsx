c9737277a6901a9983c7d2b5b029ebb2e894c33b
marwanbardaji <marwanbardaji@outlook.com>
2024-04-10 18:30:49 +0200

    Added Login Endpoint

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -8,17 +8,17 @@ export default function Navbar() {
 
       {/* Buttons */}
       <div className="flex gap-4">
-        <Link href={"https://calendly.com/alex-ckeb/15"}>
+          <a href="https://app.hiredarwin.ai/api/auth/login">
           <button className="rounded-md px-3 py-2 text-sm hover:bg-zinc-100">
             Sign in
           </button>
-        </Link>
+          </a>
 
-        <Link href={"/#"}>
+        <a href={"https://calendly.com/alex-ckeb/15"}>
           <button className="group rounded-md bg-black px-3 py-2 text-sm text-white hover:bg-blue">
             Hire Darwin <span className="inline-block tracking-normal translate-x-0 group-hover:translate-x-1 transition-transform duration-200">-&gt;</span>
           </button>
-        </Link>
+        </a>
       </div>
 
     </nav>
