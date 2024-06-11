d56daca050e36f8490e104b694dedb73e32ab9c7
Alex <alex.akten@outlook.com>
2024-06-11 16:35:24 +0200

    gap

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
       </div>
 
       {/* Buttons */}
-      <div className="col-span-1 flex whitespace-nowrap items-center justify-end gap-8">
+      <div className="col-span-1 flex whitespace-nowrap items-center justify-end gap-4">
         <Link href="/kontakt">
           <button className="rounded-md  px-3 py-2 text-sm font-medium hover:bg-zinc-100">
             Kontakta oss
