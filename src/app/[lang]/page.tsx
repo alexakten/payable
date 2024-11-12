705a5d9a30d38d4b6743fca12e948067b238d603
Alex <alex.akten@outlook.com>
2024-11-12 15:26:46 +0100

    .

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -72,7 +72,7 @@ export default async function Home({ params: { lang } }: Params) {
               </span>
             </button>
           </Link>
-          <Link href={`/${lang}/contaccontactt`}>
+          <Link href={`/${lang}/contact`}>
             <button className="text-md group rounded-md bg-zinc-100 px-4 py-3 font-normal text-black hover:bg-zinc-200">
               {dict.hero.ctaContact}{" "}
               <span className="inline-block translate-x-0 tracking-normal transition-transform duration-200 group-hover:translate-x-1">
